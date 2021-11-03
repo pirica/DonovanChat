32e3661bce5006946bdbe9221fb74c8a4bbe15d6
Donovan <90481137+therealdonovan@users.noreply.github.com>
2021-11-03 17:07:20 -0400

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,9 @@
+<?php 
+$realcode = '$3t@g00dp@$Sw0Rd!'; //Users need to know the chat code to view the chat or send a message in the chat
+$chatname = 'Donovan Chat'; //The name of the chat, will appear at the top of the page
+$chatreload = '10'; //The delay in seconds to check for updates in the chat, i reccomend leaving this as-is 
+$minmsg = "10"; //The minimum character requirement for the chat
+$maxmsg = "100"; //The maximum character requirement for the chat
+$maxsize = "100000000"; //The maximum size of the chat (in bytes), old chats will be cleared when this limit is met, currently set to 100mb (1/10 of a gb)
+$weburl = "http://localhost"; //The URL of your site, if you are using XAMPP keep this as-is
+?>
